92c7cd5b754dd5ccc32eb62e8af222a1db1294b2
Steve Derico <steve@stevederico.com>
2013-03-02 12:55:57 -0800

    Added Redcarpet and Markdown Support

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Notetaker::Application.routes.draw do
+  resources :pages
+
+  root :to => 'pages#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ Notetaker::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+
 
   # See how all your routes lay out with "rake routes"
 
