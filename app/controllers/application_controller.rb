debc64320e097c065a33975027a4d5e6059a4094
Steve Derico <steve@stevederico.com>
2013-03-13 19:06:27 -0700

    Added Password with ENV Variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authenticate
+
+	protected
+
+	def authenticate
+	  authenticate_or_request_with_http_basic do |username, password|
+	    username == "steve" && password == "steve"
+	  end
+	end
+
+
 end
