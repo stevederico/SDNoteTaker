44dc7fcea8607d285cf2c838f688f7a6978d555b
Steve Derico <steve@stevederico.com>
2013-04-28 15:44:07 -0700

    Added Password and Username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
 
+  before_filter :authenticate
 
-	protected
-
-	def authenticate
-	  authenticate_or_request_with_http_basic do |username, password|
-	    username == "steve" && password == "steve"
+  def authenticate
+		
+		if Rails.env.production?
+			authenticate_or_request_with_http_basic do |user, password|
+	   	 user == ENV['USERNAME'] && password == ENV['PASSWORD']
+	  	 end
 	  end
-	end
+
+  end
 
 
 end
